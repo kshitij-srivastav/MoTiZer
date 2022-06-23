f8866801905e9702806fd6a5877958aab61cbd7a
kshitij-srivastav <kshitij.13srivastav@gmail.com>
2022-06-24 00:36:56 +0530

    "first commit"

---
+++ app/src/main/java/com/example/motizer/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.motizer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        setContentView(R.layout.activity_main);
+        //this will bind your MainActivity.class file with activity_main.
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this,
+                        webview.class);
+
+
+                startActivity(i);
+
+
+                finish();
+
+            }
+        }, 1500);
+    }
+}
---
+++ app/src/main/java/com/example/motizer/webview.java
@@ -0,0 +1,33 @@
+package com.example.motizer;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class webview extends AppCompatActivity {
+    WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+        webView = findViewById(R.id.webview);
+        WebSettings websetting = webView.getSettings();
+        websetting.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new Callback());
+        webView.loadUrl("https://www.google.com/");
+    }
+
+    private class Callback extends WebViewClient {
+        @Override
+        public boolean shouldOverrideKeyEvent(WebView view, KeyEvent event) {
+            return false;
+        }
+    }
+}
